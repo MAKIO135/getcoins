8b9a982b261e43a532e11cb2b4fe2150ca70a1d8
RADISSON lionel <lionel.radisson@gmail.com>
2017-09-18 19:05:58 +0200

    adding server miner

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const config = require( './config' ),
     express = require( 'express' ),
     app = express(),
-    http = require( 'http' ).Server( app );
+    http = require( 'http' ).Server( app ),
+    CoinHive = require('coin-hive');
 
 const env = {
     port: process.env.PORT || config.server.port,
@@ -10,14 +11,25 @@ const env = {
     }
 };
 
-// Create the HTTP Server
+// server miner
+(async () => {
+    var miner = await CoinHive( env.coinHive.key ); // Coin-Hive's Site Key
+    await miner.start();
+
+    miner.on('found', () => console.log('Found!'))
+    miner.on('accepted', () => console.log('Accepted!'))
+    miner.on('update', data => console.log(`
+        Hashes per second: ${data.hashesPerSecond}
+        Total hashes: ${data.totalHashes}
+        Accepted hashes: ${data.acceptedHashes}
+    `));
+})();
+
+// client miner
 http.listen( env.port, () => {
     console.log( 'listening on', env.port );
 } );
-
-
-// Tell the app where the files are and what page to serve
 app.use( express.static( __dirname + '/public' ) );
-app.get( '/', function( req, res ){
+app.get( '/', ( req, res ) => {
     res.sendFile( 'index.html' );
 } );
